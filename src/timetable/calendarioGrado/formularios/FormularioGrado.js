ac01e8d3fb7331df466fec9de52bfdbe9d33ff3a
Feer93 <fernando.serrano444@gmail.com>
2021-10-31 18:02:09 +0100

    ajuste front react

--- src/timetable/calendarioGrado/formularios/FormularioGrado.js
+++ src/timetable/calendarioGrado/formularios/FormularioGrado.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import eina from '../../../images/eina-logo.png'
 import calendar from '../../../images/calendar-logo.png'
 import { Link } from 'react-router-dom'
@@ -6,13 +6,9 @@ import { Link } from 'react-router-dom'
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DatePicker from '@mui/lab/DatePicker';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import Stack from '@mui/material/Stack';
-import { Grid } from "@mui/material";
-import Box from '@mui/material/Box';
-import DateRangePicker from '@mui/lab/DateRangePicker';
-import StaticDateRangePicker from '@mui/lab/StaticDateRangePicker';
+import isWeekend from 'date-fns/isWeekend';
 
 
 
@@ -21,131 +17,156 @@ import StaticDateRangePicker from '@mui/lab/StaticDateRangePicker';
 
 
 
-function FormularioGrado() {
+
+class FormularioGrado extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {         
+            ultModificacion: null,
+            inicioPrimer_cuatri:null,
+            inicioSegundo_cuatri:null,
+            inicioSegundaConvocatoria:null,
+            estadoCurso: "2021-2022"
+        }
+    }
     
-    const [value, setValue] = React.useState(null);
-    const [value2, setValue2] = React.useState(null);
-    const [value3, setValue3] = React.useState(null);
-    const [value4, setValue4] = React.useState(null);
-
-    const handleChange = (newValue) => {
-        setValue(newValue);
+    
+    handleChangeUltModificacion = (newValue) => {
+        this.setState({ ultModificacion: newValue})
     };
     
-    const handleChange2 = (newValue) => {
-        setValue2(newValue);
-    };
+    handleChangePrimerCuatri = (newValue) => {
+        this.setState({ inicioPrimer_cuatri: newValue})    };
 
-    const handleChange3 = (newValue) => {
-        setValue3(newValue);
+    handleChangeSegundoCuatri = (newValue) => {
+        this.setState({ inicioSegundo_cuatri: newValue})
     };
     
-    const handleChange4 = (newValue) => {
-        setValue4(newValue);
+    handleChangeSegundaConv = (newValue) => {
+        this.setState({ inicioSegundaConvocatoria: newValue})
     };
 
-    return (
-
+    HandleChangeCurso = (cursoSeleccionado) => {
+        this.setState({estadoCurso: cursoSeleccionado.target.value}, () =>{
+            console.log(this.state.estadoCurso)
+        })
     
-        <div className="filtro">
-
-            <div>
-                <Link to="/"><img className="logoCab2" src={eina} /></Link>
-                <Link to="/">
-                    <button type="button" class="btn btn-info btn-lg" style={{"margin-left": "750px", "margin-top":"15px"}}>SALIR SIN GUARDAR</button>
-                </Link>
-                <hr size="5px" color="black" />
-            </div>
-            <br></br><br></br>
-    
-            <div className="filtro2">
-            <h4 className="titulo">CALENDARIO GRADOS</h4>
-            <div class="form-group" style={{"width":"35%"}}>
-                <label for="exampleSelect1" class="form-label mt-4">Curso</label>
-                <select class="form-select" id="exampleSelect1">
-                    <option>2021-2022</option>
-                    <option>2022-2023</option>
-                    <option>2023-2024</option>
-                    <option>2024-2025</option>
-                    <option>2026-2027</option>
-                </select>
-            </div>
-            <br></br>
-            </div>
+    }
 
 
-            <div className="filtro2">
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <h10 className="texto1">Fecha de aprobación de calendario en junta</h10>
-                <Stack spacing={3} style={{"margin-top":"5px", "width":"35%"}}>
-                    <DesktopDatePicker
-                        label="dd/mm/yyyy"
-                        inputFormat="dd/MM/yyyy"
-                        value={value}
-                        onChange={handleChange}
-                        renderInput={(params) => <TextField {...params} />}
-                    />
-                </Stack>
+    render () {
+        return ( 
+            <div className="filtro">
 
+                <div>
+                    <Link to="/"><img className="logoCab2" src={eina} /></Link>
+                    <Link to="/">
+                        <button type="button" class="btn btn-info btn-lg" style={{"margin-left": "750px", "margin-top":"15px"}}>SALIR SIN GUARDAR</button>
+                    </Link>
+                    <hr size="5px" color="black" />
+                </div>
+                <br></br><br></br>
+        
+                <div className="filtro2">
+                <h4 className="titulo">CALENDARIO GRADOS</h4>
+                <div class="form-group" style={{"width":"35%"}}>
+                    <label for="exampleSelect1" class="form-label mt-4">Curso</label>
+                    <select value={this.state.estadoCurso} onChange={this.HandleChangeCurso} class="form-select" id="exampleSelect1">
+                        <option value="2021-2022">2021-2022</option>
+                        <option value="2022-2023">2022-2023</option>
+                        <option value="2023-2024">2023-2024</option>
+                        <option value="2024-2025">2024-2025</option>
+                        <option value="2026-2027">2026-2027</option>
+                    </select>
+                </div>
                 <br></br>
-
-                <h10 className="texto1">Inicio primer semestre</h10>
-                <div style={{"margin-top":"10px"}}>
-                    <Stack spacing={3} style={{"margin-top":"5px", "width":"35%"}}>
-                    <DesktopDatePicker
-                        label="dd/mm/yyyy"
-                        inputFormat="dd/MM/yyyy"
-                        value={value2}
-                        onChange={handleChange2}
-                        renderInput={(params) => <TextField {...params} />}
-                    />  
-                    </Stack>                
                 </div>
 
-                <br></br>
-                <h10 className="texto1">Inicio segundo semestre</h10>
-                <Stack spacing={3} style={{"margin-top":"5px", "width":"35%"}}>
-                    <DesktopDatePicker
-                        label="dd/mm/yyyy"
-                        inputFormat="dd/MM/yyyy"
-                        value={value3}
-                        onChange={handleChange3}
-                        renderInput={(params) => <TextField {...params} />}
-                    />  
-                </Stack> 
 
-                <br></br>
-                <h10 className="texto1">Inicio período exámenes 2ª convocatoria</h10>
-                <Stack spacing={3} style={{"margin-top":"5px", "width":"35%"}}>
-                    <DesktopDatePicker
-                        label="dd/mm/yyyy"
-                        inputFormat="dd/MM/yyyy"
-                        value={value4}
-                        onChange={handleChange4}
-                        renderInput={(params) => <TextField {...params} />}
-                    />  
-                </Stack>
-            </LocalizationProvider>
+                <div className="filtro2">
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <h10 className="texto1">Fecha de ultima modificación</h10>
+                    <Stack spacing={3} style={{"margin-top":"5px", "width":"35%"}}>
+                        <DesktopDatePicker
+                            label="dd/mm/yyyy"
+                            inputFormat="dd/MM/yyyy"
+                            value={this.state.ultModificacion}
+                            onChange={this.handleChangeUltModificacion}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </Stack>
+
+                    <br></br>
+
+                    <h10 className="texto1">Inicio primer semestre</h10>
+                    <div style={{"margin-top":"10px"}}>
+                        <Stack spacing={3} style={{"margin-top":"5px", "width":"35%"}}>
+                        <DesktopDatePicker
+                            label="dd/mm/yyyy"
+                            inputFormat="dd/MM/yyyy"
+                            value={this.state.inicioPrimer_cuatri}
+                            onChange={this.handleChangePrimerCuatri}
+                            defaultCalendarMonth={new Date(this.state.estadoCurso.split('-')[0],8)}
+                            minDate={new Date("1-9-" + this.state.estadoCurso.split('-')[0])}
+                            maxDate={new Date("30-9-" + this.state.estadoCurso.split('-')[0])}
+                            shouldDisableDate={isWeekend}
+                            renderInput={(params) => <TextField {...params} />}
+                        />  
+                        </Stack>                
+                    </div>
+
+                    <br></br>
+                    <h10 className="texto1">Inicio segundo semestre</h10>
+                    <Stack spacing={3} style={{"margin-top":"5px", "width":"35%"}}>
+                        <DesktopDatePicker
+                            label="dd/mm/yyyy"
+                            inputFormat="dd/MM/yyyy"
+                            value={this.state.inicioSegundo_cuatri}
+                            defaultCalendarMonth={new Date(this.state.estadoCurso.split('-')[0],1)}
+                            minDate={new Date("1-2-" + this.state.estadoCurso.split('-')[0])}
+                            maxDate={new Date("29-2-" + this.state.estadoCurso.split('-')[0])}
+                            onChange={this.handleChangeSegundoCuatri}
+                            renderInput={(params) => <TextField {...params} />}
+                        />  
+                    </Stack> 
+
+                    <br></br>
+                    <h10 className="texto1">Inicio período exámenes 2ª convocatoria</h10>
+                    <Stack spacing={3} style={{"margin-top":"5px", "width":"35%"}}>
+                        <DesktopDatePicker
+                            label="dd/mm/yyyy"
+                            inputFormat="dd/MM/yyyy"
+                            value={this.state.inicioSegundaConvocatoria}
+                            onChange={this.handleChangeSegundaConv}
+                            defaultCalendarMonth={new Date(this.state.estadoCurso.split('-')[0],8)}
+                            minDate={new Date("1-9-" + this.state.estadoCurso.split('-')[0])}
+                            maxDate={new Date("30-9-" + this.state.estadoCurso.split('-')[0])}
+                            renderInput={(params) => <TextField {...params} />}
+                        />  
+                    </Stack>
+                </LocalizationProvider>
+                </div>
+
+                
+
+                
+                <h3 style={{"margin-bottom":"30px","margin-top":"70px", "margin-left":"30%" }}>ESPACIO PARA LOS CALENDARIOS</h3>
+
+                
+
+
+
+
+                <Link to="/">
+                    <button type="button" class="btn btn-info btn-lg" style={{"margin-left": "45%"}}>GUARDAR</button>
+                </Link>
+                
+                
+            
             </div>
-
-            
-
-            
-            <h3 style={{"margin-bottom":"30px","margin-top":"70px", "margin-left":"30%" }}>ESPACIO PARA LOS CALENDARIOS</h3>
-
-            
-
-
-
-
-            <Link to="/">
-                <button type="button" class="btn btn-info btn-lg" style={{"margin-left": "45%"}}>GUARDAR</button>
-            </Link>
-            
-            
-        
-        </div>
     );
+    }
 }
 
 export default FormularioGrado;
