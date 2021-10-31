4994b3c813989c41d2aaae23f7d8718cd76b2149
Javimiko <72088626+Javimiko@users.noreply.github.com>
2021-10-31 17:58:04 +0100

    develop

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import App2 from './timetable/calendarioGrado/calendarioGrado'
-import Raiz from './root/Raiz'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Raiz />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
