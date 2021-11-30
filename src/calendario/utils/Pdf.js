edffe5187e13df0e6074ba53b55c3cd734f1d50b
Feer93 <fernando.serrano444@gmail.com>
2021-12-01 00:02:56 +0100

    corregido pdf segun zoom

--- src/calendario/utils/Pdf.js
+++ src/calendario/utils/Pdf.js
@@ -7,12 +7,15 @@ import Calendario from "../CalendarioGrado";
 
 class Pdf {
      static printDocument(id) {
+         
         const input = document.getElementById(id);
         html2canvas(input)
             .then((canvas) => {
                 const imgData = canvas.toDataURL('image/png');
-                const pdf = new jsPDF();
-                pdf.addImage(imgData, 'JPEG', 0, 0);
+                const pdf = new jsPDF("p", "mm", "a4");
+                var width = pdf.internal.pageSize.getWidth();
+                var height = pdf.internal.pageSize.getHeight();
+                pdf.addImage(imgData, 'JPEG', 0, 0,width, height);
                 pdf.save("calendar.pdf");
             })
         ;
