df8fb63ce015024e1c266eb2915d03caf18193eb
diego <755232@unizar.es>
2022-01-05 13:30:51 +0100

    Add clase a horario

--- src/horario/Horario.js
+++ src/horario/Horario.js
@@ -6,16 +6,8 @@ import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import {Alert} from "@material-ui/lab";
 import {Link} from "react-router-dom";
 import eina from "../images/eina-logo.png";
-import Timetable from 'react-scheduler-table'
-
-const settings = {
-    cellHeight: 50,
-    startDay: "08:00",
-    endDay: "21:00",
-    is12hours: false,
-    hourSplit: 1, // 1 hour / 0.25 = 15 min - each row
-    columnCnt: 5
-};
+import './Horario.css'
+import constants from "./utils/Constants";
 
 class Horario extends Component {
 
@@ -25,14 +17,22 @@ class Horario extends Component {
         this.state = {
             //Estado de la clase seleccionada
             claseSelected: {
-                "dia": "",
-                "hora": null,
-                "asignatura": "",
-                "duracion": null,
-                "tipo": ""
+                dia: "",
+                hora: null,
+                asignatura: "",
+                duracion: null,
+                tipo: ""
             },
             //Clases insertadas en el horario
-            horario_changed: [],
+            horario: [
+                {
+                    dia: "Lunes",
+                    hora: "8",
+                    asignatura: "Naturales",
+                    duracion: 30,
+                    tipo: "teoria"
+                },
+            ],
             //Asignaturas disponibles
             asignaturasOptions: [],
             //Estado del formulario
@@ -66,7 +66,7 @@ class Horario extends Component {
      * @returns [Object] las opciones del select 'dia'
      */
     getDiasOptions() {
-        var dias = ["L", "M", "M", "J", "V", "S", "D"]
+        var dias = constants.DAYS
         return dias.map((d) => ({"value": d, "label": d}))
     }
 
@@ -129,10 +129,30 @@ class Horario extends Component {
      *              Puede existir en el horario una clase solapada en el tiempo con la que se quiere añadir.
      */
     eliminarRedundancias(new_clase) {
-        return this.state.horario_changed.filter(
+        return this.state.horario.filter(
             (clase) => !this.asignaturasIguales(clase, new_clase)
                 && !this.clasesSolapadas(clase, new_clase)
         );
+    }
+
+    /**
+     *
+     * @param claseSelected Objeto que guarda el estado del formulario
+     * Devuelve un objeto clase
+     */
+     newClase(claseSelected) {
+         var hora = (claseSelected.hora.getMinutes() === 0)
+             ? claseSelected.hora.getHours().toString()
+             : claseSelected.hora.getHours() + ":" + claseSelected.hora.getMinutes()
+         var clase = {
+            dia: claseSelected.dia.label,
+            hora: hora,
+            asignatura: claseSelected.asignatura.label,
+            duracion: claseSelected.duracion,
+            tipo: claseSelected.tipo
+        }
+        console.log("newclse: " + claseSelected.dia.label)
+        return clase
     }
 
     /**
@@ -145,125 +165,152 @@ class Horario extends Component {
             return
         } else {
             //Caso formulario rellenado correctamente
+            var newClase = this.newClase(this.state.claseSelected)
             //Eliminamos solapamientos entre clases y asignaturas repetidas
-            this.state.horario_changed = this.eliminarRedundancias(this.state.claseSelected)
+            this.state.horario = this.eliminarRedundancias(newClase)
             //Anadimos la nueva clase al horario
-            this.state.horario_changed.push(this.state.claseSelected)
-            this.setState({formError: false})
+            this.setState({
+                formError: false,
+                horario: [...this.state.horario, newClase],
+            })
         }
     }
 
-    render() {
+    //html de la cabecera con el logo y titulo
+    htmlCabecera() {
+        return(
+            <div>
+                <Link to="/"><img className="logoCabecera" src={eina} /></Link>
+                <Link to="/">
+                    <button type="button" className="btn btn-info btn-lg" style={{ "margin-left": "750px", "margin-top": "15px" }}>SALIR SIN GUARDAR</button>
+                </Link>
+                <hr size="5px" color="black" />
+            </div>
+        )
+    }
 
-        return (
-            <><><div>
-
-                <div>
-                    <Link to="/"><img className="logoCabecera" src={eina} /></Link>
-                    <Link to="/">
-                        <button type="button" className="btn btn-info btn-lg" style={{ "margin-left": "750px", "margin-top": "15px" }}>SALIR SIN GUARDAR</button>
-                    </Link>
-                    <hr size="5px" color="black" />
-                </div> <br></br>
-
-                <div style={{
-                    display: "block",
-                    margin: "auto",
-                    width: "50%",
-                    border: "1px solid #b8b894",
-                    borderRadius: "8px",
-                    padding: "10px",
-                    boxShadow: "4px 5px 4px -3px rgba(97,97,97,1)"
-                }}>
-                    <p>Añadir asignatura</p>
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        margin: "auto",
-                        justifyContent: "center",
-                        flexWrap: "wrap"
-                    }}>
-                        <div style={{ margin: "6px" }}>
-                            <Select
-                                options={this.state.asignaturasOptions}
-                                placeholder={<div>Asignatura</div>}
-                                onChange={(a) => {
-                                    this.setState(this.state.claseSelected.asignatura = a);
-                                } }
-                                value={this.state.claseSelected.asignatura} />
-                        </div>
-                        <div style={{ margin: "6px" }}>
-                            <Select
-                                options={this.getDiasOptions()}
-                                placeholder={<div>Día</div>}
-                                onChange={(d) => {
-                                    this.setState(this.state.claseSelected.dia = d);
-                                } }
-                                value={this.state.claseSelected.dia} />
-                        </div>
-                        <div style={{ margin: "6px" }}>
-                            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                <TimePicker style={{ boxSizing: "border-box" }}
-                                    placeholder="Hora"
-                                    minutesStep={30}
-                                    minTime={new Date(0, 0, 0, 8)}
-                                    maxTime={new Date(0, 0, 0, 20, 30)}
-                                    onChange={(h) => {
-                                        this.setState(this.state.claseSelected.hora = h);
-                                    } }
-                                    value={this.state.claseSelected.hora}
-                                    renderInput={(params) => <TextField {...params} />} />
-                            </LocalizationProvider>
-                        </div>
-                        <div style={{ margin: "6px" }}>
-                            <Select
-                                options={this.getDuracionOptions()}
-                                placeholder={<div>Duración</div>}
-                                onChange={(d) => {
-                                    this.setState(this.state.claseSelected.duracion = d);
-                                } }
-                                value={this.state.claseSelected.duracion} />
-                        </div>
-                        <div style={{ margin: "6px" }}>
-                            <Select
-                                options={this.getTipoOptions()}
-                                placeholder={<div>Tipo</div>}
-                                onChange={(t) => {
-                                    this.setState(this.state.claseSelected.tipo = t);
-                                } }
-                                value={this.state.claseSelected.tipo} />
-                        </div>
+    //html con el formulario para anhadir una asignatura
+     htmlFormulario() {
+        return(
+            <div className="fondoFormulario">
+                <p>Añadir asignatura</p>
+                <div className="contenidoFormulario">
+                    <div className="elementoFormulario">
+                        <Select
+                            options={this.state.asignaturasOptions}
+                            placeholder={<div>Asignatura</div>}
+                            onChange={(a) => {
+                                this.setState(this.state.claseSelected.asignatura = a);
+                            } }
+                            value={this.state.claseSelected.asignatura} />
                     </div>
-                    <button className="btn btn-info" id="closeDialog" onClick={() => this.addClase()}>Añadir
-                    </button>
-                    {this.state.formError
-                        ? (<div style={{ marginTop: "6px" }}><Alert severity="error">Algún campo está vacío — ¡Todos son
-                            obligatorios!</Alert></div>)
-                        : ''}
+                    <div className="elementoFormulario">
+                        <Select
+                            options={this.getDiasOptions()}
+                            placeholder={<div>Día</div>}
+                            onChange={(d) => {
+                                this.setState(this.state.claseSelected.dia = d);
+                            } }
+                            value={this.state.claseSelected.dia} />
+                    </div>
+                    <div className="elementoFormulario">
+                        <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <TimePicker style={{ boxSizing: "border-box" }}
+                                        placeholder="Hora"
+                                        minutesStep={30}
+                                        minTime={new Date(0, 0, 0, 8)}
+                                        maxTime={new Date(0, 0, 0, 20, 30)}
+                                        onChange={(h) => {
+                                            this.setState(this.state.claseSelected.hora = h);
+                                        } }
+                                        value={this.state.claseSelected.hora}
+                                        renderInput={(params) => <TextField {...params} />} />
+                        </LocalizationProvider>
+                    </div>
+                    <div className="elementoFormulario">
+                        <Select
+                            options={this.getDuracionOptions()}
+                            placeholder={<div>Duración</div>}
+                            onChange={(d) => {
+                                this.setState(this.state.claseSelected.duracion = d);
+                            } }
+                            value={this.state.claseSelected.duracion} />
+                    </div>
+                    <div className="elementoFormulario">
+                        <Select
+                            options={this.getTipoOptions()}
+                            placeholder={<div>Tipo</div>}
+                            onChange={(t) => {
+                                this.setState(this.state.claseSelected.tipo = t);
+                            } }
+                            value={this.state.claseSelected.tipo} />
+                    </div>
                 </div>
+
+                <button className="btn btn-info" onClick={() => this.addClase()}>Añadir</button>
+
+                {this.state.formError
+                    ? (<div className="elementoFormulario"><Alert severity="error">Algún campo está vacío — ¡Todos son
+                        obligatorios!</Alert></div>)
+                    : ''}
+            </div>
+        )
+    }
+
+    /**
+     *
+     * @param hora
+     * @param dia
+     * Devuelve una celda html con la asignatura perteneciente
+     * a un dia y hora del horario
+     */
+    htmlClase(hora, dia) {
+        let clase =  this.state.horario.find(clase =>
+             clase.hora == hora && clase.dia == dia
+        )
+        return(
+            <td>{(clase) ? clase.asignatura : ""}</td>
+        )
+    }
+
+    htmlHorario() {
+        return(
+            <table className="timetable">
+                <thead style={{fontWeight: 'bold'}}>
+                <tr>
+                    <td className="columnaHoras">Horas</td>
+                    {constants.DAYS.map((day) => (
+                        <td key={day} >
+                            <div style={{textAlign: 'center'}}>
+                                {day}
+                            </div>
+                        </td>
+                    ))}
+                </tr>
+                </thead>
+                <tbody>
+                {constants.RANGO_HORAS.map((hora, index) => (
+                    <tr>
+                        <td> {hora}</td>
+                        {this.htmlClase(hora, constants.DIAS_SEMANA.LUNES)}
+                        {this.htmlClase(hora, constants.DIAS_SEMANA.MARTES)}
+                        {this.htmlClase(hora, constants.DIAS_SEMANA.MIERCOLES)}
+                        {this.htmlClase(hora, constants.DIAS_SEMANA.JUEVES)}
+                        {this.htmlClase(hora, constants.DIAS_SEMANA.VIERNES)}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        )
+    }
+    render() {
+        return (
+            <div>
+                {this.htmlCabecera()}
+                {this.htmlFormulario()}
+                {this.htmlHorario()}
 
             </div>
-                <div class="timetable" style={{
-                    marginTop: "20px", 
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                    marginBottom: "auto",
-                    display: "block",
-                    width: "75%",
-                    border: "1px solid #b8b894",
-                    borderRadius: "8px",
-                    padding: "10px",
-                    boxShadow: "4px 5px 4px -3px rgba(97,97,97,1)"
-                }} >
-                    <Timetable settings={settings} />
-                </div>
-            </><script>
-                    document.getElementById("1").textContent="Lunes";
-                    document.getElementById("2").textContent="Martes";
-                    document.getElementById("3").textContent="Miercoles";
-                    document.getElementById("4").textContent="Jueves";
-                    document.getElementById("5").textContent="Viernes";
-                </script></>
 
         )
 
---
+++ src/horario/utils/Constants.js
@@ -0,0 +1,24 @@
+/**
+ * Constantes que usa la aplicación por mantener cierta modularidad y que no sea un caos
+ * @module Constants
+ */
+ const constants = {
+    // Dias de la semana laborales
+    DIAS_SEMANA : {
+        LUNES:     "Lunes",
+        MARTES:    "Martes",
+        MIERCOLES: "Miércoles",
+        JUEVES:    "Jueves",
+        VIERNES:   "Viernes",
+    },
+    //Cabecera del horario
+    DAYS : ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes"],
+
+    RANGO_HORAS: ["8", "8:30", "9", "9:30", "10", "10:30", "11", "11:30",
+        "12", "12:30", "13", "13:30", "14", "14:30", "15:00", "15:30", "16",
+        "16:30", "17", "17:30", "18", "18:30","19", "19:30", "20", "20:30",
+    ]
+
+}
+
+export default constants
