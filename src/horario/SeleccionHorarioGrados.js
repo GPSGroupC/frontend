0a89e92a9ce8fd2961d3f7a941040f1a0084b26c
Guille Cánovas <755848@unizar.es>
2022-01-11 23:52:35 +0100

    falta pasar id a la pantalla 'editar-horario'

--- src/horario/SeleccionHorarioGrados.js
+++ src/horario/SeleccionHorarioGrados.js
@@ -129,127 +129,7 @@ function SeleccionHorarioGrados() {
     setAnchorEl(null);
   };
 
-  //Ingenieria de Diseño
-  const [anchorEl2, setAnchorEl2] = React.useState(null);
-  const open2 = Boolean(anchorEl2);
-  const handleClick2 = (event) => {
-    setAnchorEl2(event.currentTarget);
-  };
-  const handleClose2 = () => {
-    setAnchorEl2(null);
-  };
-
-  //Ingenieria electrica 
-  const [anchorEl3, setAnchorEl3] = React.useState(null);
-  const open3 = Boolean(anchorEl3);
-  const handleClick3 = (event) => {
-    setAnchorEl3(event.currentTarget);
-  };
-  const handleClose3 = () => {
-    setAnchorEl3(null);
-  };
-
-
-  //Ingenieria mecanica
-  const [anchorEl4, setAnchorEl4] = React.useState(null);
-  const open4 = Boolean(anchorEl4);
-  const handleClick4 = (event) => {
-    setAnchorEl4(event.currentTarget);
-  };
-  const handleClose4 = () => {
-    setAnchorEl4(null);
-  };
-
-  //Ingenieria quimica
-  const [anchorEl5, setAnchorEl5] = React.useState(null);
-  const open5 = Boolean(anchorEl5);
-  const handleClick5 = (event) => {
-    setAnchorEl5(event.currentTarget);
-  };
-  const handleClose5 = () => {
-    setAnchorEl5(null);
-  };
-
-  //Ingenieria industrial
-  const [anchorEl6, setAnchorEl6] = React.useState(null);
-  const open6 = Boolean(anchorEl6);
-  const handleClick6 = (event) => {
-    setAnchorEl6(event.currentTarget);
-  };
-  const handleClose6 = () => {
-    setAnchorEl6(null);
-  };
-
-  //Ingenieria teleco
-  const [anchorEl7, setAnchorEl7] = React.useState(null);
-  const open7 = Boolean(anchorEl7);
-  const handleClick7 = (event) => {
-    setAnchorEl7(event.currentTarget);
-  };
-  const handleClose7 = () => {
-    setAnchorEl7(null);
-  };
-
-  //Ingenieria electronica
-  const [anchorEl8, setAnchorEl8] = React.useState(null);
-  const open8 = Boolean(anchorEl8);
-  const handleClick8 = (event) => {
-    setAnchorEl8(event.currentTarget);
-  };
-  const handleClose8 = () => {
-    setAnchorEl8(null);
-  };
-
-  //Arquitectura
-  const [anchorEl9, setAnchorEl9] = React.useState(null);
-  const open9 = Boolean(anchorEl9);
-  const handleClick9 = (event) => {
-    setAnchorEl9(event.currentTarget);
-  };
-  const handleClose9 = () => {
-    setAnchorEl9(null);
-  };
-
-  //optativas
-  const [anchorEl10, setAnchorEl10] = React.useState(null);
-  const open10 = Boolean(anchorEl10);
-  const handleClick10 = (event) => {
-    setAnchorEl10(event.currentTarget);
-  };
-  const handleClose10 = () => {
-    setAnchorEl10(null);
-  };
-
-  //rotadas
-  const [anchorEl11, setAnchorEl11] = React.useState(null);
-  const open11 = Boolean(anchorEl11);
-  const handleClick11 = (event) => {
-    setAnchorEl11(event.currentTarget);
-  };
-  const handleClose11 = () => {
-    setAnchorEl11(null);
-  };
-
-  //infmat
-  const [anchorEl12, setAnchorEl12] = React.useState(null);
-  const open12 = Boolean(anchorEl12);
-  const handleClick12 = (event) => {
-    setAnchorEl12(event.currentTarget);
-  };
-  const handleClose12 = () => {
-    setAnchorEl12(null);
-  };
-
-  //quimica consec
-  const [anchorEl13, setAnchorEl13] = React.useState(null);
-  const open13 = Boolean(anchorEl13);
-  const handleClick13 = (event) => {
-    setAnchorEl13(event.currentTarget);
-  };
-  const handleClose13 = () => {
-    setAnchorEl13(null);
-  };
-
+ 
   const handleAddFormSubmit = (event, codplan) => {
     event.preventDefault();
 
@@ -289,8 +169,7 @@ function SeleccionHorarioGrados() {
     <div className="HorarioSeleccion">
 
       <div style={{ "margin-left": "37.5%" }} >
-        <button type="button" class="btn btn-info btn-md" style={{ "margin-left": "70px" }}>GRADO</button>
-        <Link to=""><button type="button" class="btn btn-outline-info">MASTER</button></Link>
+        <button type="button" class="btn btn-info btn-md" style={{ "margin-left": "70px" }}>GRADOS Y MASTERS</button>
       </div> <br></br>
 
 
@@ -303,13 +182,12 @@ function SeleccionHorarioGrados() {
               {
                 openGradoId === grado.codplan ? (
                 horarios.map(horario => (grado.codplan === horario.codplan) ? 
-                <div class="contenedor" style={{ width: '500px', marginLeft: '32.5%', marginBottom: '5px' }}>
-                  <div class="contenido"><MenuItem component={Link} to="/editar-horario" disableRipple> {horario.grupo}-{horario.periodo} {grado.nombre}. {horario.curso}º {horario.descripcion} </MenuItem></div>
-                  <div class="contenido"><button style={{padding:"0px", backgroundColor:"white", color:"dimgrey"}}id="delButton"onClick={(e) => handleDelHorario(e, horario)}><DeleteSharpIcon></DeleteSharpIcon></button></div>
+                <div class="contenedor" style={{ width: '500px', marginLeft: '32.5%' }}>
+                  <div class="contenido" style={{ width:'700px', alignItems:'center' ,fontWeight: 'bold'}}><MenuItem component={Link} to={'/editar-horario/'}> {horario.grupo}-{horario.periodo} {grado.nombre}. {horario.curso}º {horario.descripcion} <button style={{padding:"0px", backgroundColor:"white", color:"dimgrey"}}id="delButton"onClick={(e) => handleDelHorario(e, horario)}><DeleteSharpIcon></DeleteSharpIcon></button> </MenuItem></div>
                 </div> : null)) : (null)
               }
               {openGradoId === grado.codplan ? (
-              <form onSubmit={(event) => handleAddFormSubmit(event,grado.codplan)} style={{ width: '500px', marginLeft: '32.5%', marginBottom: '5px' }}>
+              <form onSubmit={(event) => handleAddFormSubmit(event,grado.codplan)} style={{ width: '500px', marginLeft: '32.5%', marginBottom: '50px', marginTop: '10px' }}>
                 <input style={{ width: "75px", marginLeft: '3%', marginRight: '5px' }}
                   type="number"
                   min="0"
